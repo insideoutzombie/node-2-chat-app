cef970079c88955cc707f94cc407abd9ef708846
insideoutzombie <david@web3devs.com>
2018-10-16 14:04:40 -0500

    add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Debo',
+    text: 'ballin so hard',
+    createdAt: 1234567
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
