1c5e19f53f5135fc8e4864a76b1a656022ebd5bd
insideoutzombie <david@web3devs.com>
2018-10-22 17:23:49 -0500

    Add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -15,13 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    io.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
@@ -30,5 +36,5 @@ socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 });
 
 server.listen(port, () => {
-  console.log('Server is up on ${port}');
+  console.log(`Server is up on ${port}`);
 });
