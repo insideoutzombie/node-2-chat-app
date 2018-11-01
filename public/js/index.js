59ad0a624c980c3c1bcf32282ba82850445eef9c
insideoutzombie <david@web3devs.com>
2018-11-01 12:07:35 -0500

    add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
@@ -26,3 +36,38 @@ jQuery('#message-form').on('submit', function (e) {
 
   });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
+  });
+});
+
+// const locationButton = jQuery('#send-location');
+// locationButton.on('click', () => {
+//
+//  if (!navigator.geolocation) {
+//  return alert('Geo location not supported by your browser');
+//  }
+//  navigator.geolocation.getCurrentPosition(function () {}, function () {}, {});
+//  navigator.geolocation.getCurrentPosition((position) => {
+//  console.log('ay');
+//  console.log(position);
+//  }, (err) => {
+//  alert('Unable to fetch location');
+//  }, {
+//  maximumAge: 60000,
+//  timeout: 10000,
+//  enableHighAccuracy: true
+//  });
+// });
